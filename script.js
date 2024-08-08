22f63b2b51669edfc836ea85df8b2c03a5b84f34
mrharshvashist <166190779+mrharshvashist@users.noreply.github.com>
2024-08-08 14:40:25 +0530

    files loaded

---
+++ script.js
@@ -0,0 +1,10 @@
+var r = document.getElementById("rememberMe");
+var c = document.getElementById("circle");
+
+
+function move() {
+        r.style.justifyContent = "end";
+        r.style.backgroundColor = "#24d84b";
+
+
+}
